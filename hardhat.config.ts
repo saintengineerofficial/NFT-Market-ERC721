e4b55941dbe1df13e94f9e391f409c4b46e63e7e
saintengineerofficial <saintengineerofficial@gmail.com>
2025-10-19 16:39:49 +0800

    Revert "feat: layerzore"
    This reverts commit 266aa6bb81e36ee0da295c2666c0243ef9537d20.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,74 +10,8 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'localhost',
   networks: {
     hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      accounts,
-    },
     BitTorrent: {
       url: "https://pre-rpc.bt.io/",
-      accounts,
-      gasPrice: 1000000000,
-    },
-    // LayerZero 测试网配置
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY || ''}`,
-      accounts,
-      gasPrice: 20000000000,
-    },
-    bscTestnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      accounts,
-      gasPrice: 10000000000,
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY || ''}`,
-      accounts,
-      gasPrice: 20000000000,
-    },
-    fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts,
-      gasPrice: 25000000000,
-    },
-    arbitrumGoerli: {
-      url: "https://goerli-rollup.arbitrum.io/rpc",
-      accounts,
-      gasPrice: 100000000,
-    },
-    optimismGoerli: {
-      url: "https://goerli.optimism.io",
-      accounts,
-      gasPrice: 1000000000,
-    },
-    // LayerZero 主网配置
-    ethereum: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY || ''}`,
-      accounts,
-      gasPrice: 20000000000,
-    },
-    bsc: {
-      url: "https://bsc-dataseed.binance.org",
-      accounts,
-      gasPrice: 5000000000,
-    },
-    polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY || ''}`,
-      accounts,
-      gasPrice: 30000000000,
-    },
-    avalanche: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      accounts,
-      gasPrice: 25000000000,
-    },
-    arbitrum: {
-      url: "https://arb1.arbitrum.io/rpc",
-      accounts,
-      gasPrice: 100000000,
-    },
-    optimism: {
-      url: "https://mainnet.optimism.io",
       accounts,
       gasPrice: 1000000000,
     },
